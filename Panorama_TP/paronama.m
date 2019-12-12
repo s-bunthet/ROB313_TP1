57c11f0c62c082ae2b48cf848094fcdf871250dc
bunthet01 <bunthetsay@gmail.com>
2019-12-12 21:40:07 +0100

    new new

--- Panorama_TP/paronama.m
+++ Panorama_TP/paronama.m
@@ -1,18 +1,18 @@
 
 % display the original images
 img1 = double(imread('Amst-3.jpg'))/255;
-f1   = figure;
+f1 = figure;
 figure(f1);
 imagesc(img1)
 
 img2 = double(imread('Amst-2.jpg'))/255;
-f2   = figure;
+f2 = figure;
 figure(f2);
 imagesc(img2)
 
 % put the first image in bbox1
-bbox1       = [-800 600 -50 450  ];
-H1          = eye(3);
+bbox1 = [-900 500 -50 450  ];
+H1 = eye(3);
 img_warped1 = vgg_warp_H(img1, H1, 'linear', bbox1);
 
 %% display the first warped image
@@ -23,26 +23,31 @@ imagesc(img_warped1)
 % put the seconde image in bbox2
 
 %% define Pt0 and PtD
+% 4 windows version 
+
 % original points
-X1 = [439.67 112.19 1];
-X2 = [499.58 108.32 1];
-X3 = [439.67 215.82 1];
-X4 = [502 216.79 1];
+X1 = [404.2 154.65 1];
+X2 = [423.69 154.22 1];
+X3 = [404.2 185.99 1];
+X4 = [424.69 186.85  1];
 
 % target points 
-X1_prime = [60.638 111.22 1];
-X2_prime = [122.85 113.15 1];
-X3_prime = [58.534 211.79  1];
-X4_prime = [127.46 213.72  1];
+X1_prime = [23.484 152.07 1];
+X2_prime = [44.653 151.64 1];
+X3_prime = [22.476 185.56 1];
+X4_prime = [43.981 185.56  1];
 
-PtO         = [X1' X2' X3' X4'];
-PtD         = [X1_prime' X2_prime' X3_prime' X4_prime'];
-H2          = homography2d(PtO, PtD);
-bbox2       = [-800 600 -50 450  ];
+PtO = [X1' X2' X3' X4'];
+PtD = [X1_prime' X2_prime' X3_prime' X4_prime'];
+H2 = homography2d(PtO, PtD);
+bbox2 = [-900 500 -50 450  ];
 img_warped2 = vgg_warp_H(img2, H2, 'linear', bbox2);
-
 %% display the first warped image
 f4 = figure;
 figure(f4);
 imagesc(img_warped2)
 % merge the two boxes 
+im_fused = max(img_warped1,img_warped2);
+f5 = figure;
+figure(f5);
+imagesc(im_fused)
